ad786b8730a9c36d466b6ecde85ee93bdd00e8f6
Murat Istek <messi897@gmail.com>
2023-05-13 16:18:37 -0400

    Configure express server with morgan and eslint

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello")
+import express from 'express'
+import morgan from 'morgan'
+
+const app = express()
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.json({ test: "server with json" })
+})
+
+const port = Number(process.env.PORT) || 8080
+
+// In Docker, whenever you are attaching to a port in your server (express), always provide a second argument '0.0.0.0'
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://localhost:${port}`)
+})
